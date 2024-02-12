79b63b01fc28776c9c14e79d57616a15e49a326f
Ali Azouz <132236484+Azouz8@users.noreply.github.com>
2024-02-12 20:29:02 +0200

    Adding Assets and Initialize the Project

---
+++ lib/views/dashboard_view.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:responsive_dashboard/layouts/desktop_layout.dart';
+import 'package:responsive_dashboard/widgets/adaptive_layout.dart';
+
+class DashBoardView extends StatelessWidget {
+  const DashBoardView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: AdaptiveLayout(
+        mobileLayout: (context) => const SizedBox(),
+        tabletLayout: (context) => const SizedBox(),
+        desktopLayout: (context) => const DashboardDesktopLayout(),
+      ),
+    );
+  }
+}
